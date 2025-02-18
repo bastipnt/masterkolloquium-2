0928e195af433fbf1c9539f95254795ac0a19cfa
litc0de <litc0de@posteo.org>
2025-02-18 09:05:14 +0100

    add names for images

--- src/slides/Mitigation.svelte
+++ src/slides/Mitigation.svelte
@@ -11,27 +11,73 @@
   <section id="ad-blockers">
     <h3 class="text-left">Ad Blockers:</h3>
     <div class="grid grid-cols-3 grid-rows-3 justify-items-center w-full gap-8">
-      <img class="w-24" src="/img/adblockers/adblock.png" alt="adblock" />
-      <img class="w-24" src="/img/adblockers/adblockplus.png" alt="adblockplus" />
-      <img class="w-24" src="/img/adblockers/ublockorigin.png" alt="ublockorigin" />
-      <img class="w-24" src="/img/adblockers/privacy-badger.png" alt="privacy-badger" />
-      <img class="w-24" src="/img/adblockers/ghostery.png" alt="ghostery" />
-      <img class="w-24" src="/img/adblockers/adguard.png" alt="adguard" />
-      <img class="w-24" src="/img/adblockers/adlock.png" alt="adlock" />
+      <div class="flex flex-col align-middle items-center gap-2">
+        <img class="w-24" src="/img/adblockers/adblock.png" alt="adblock" />
+        <p class="text-2xl">AdBlock</p>
+      </div>
+      <div class="flex flex-col align-middle items-center gap-2">
+        <img class="w-24" src="/img/adblockers/adblockplus.png" alt="adblockplus" />
+        <p class="text-2xl">AdBlock Plus</p>
+      </div>
+      <div class="flex flex-col align-middle items-center gap-2">
+        <img class="w-24" src="/img/adblockers/ublockorigin.png" alt="ublockorigin" />
+        <p class="text-2xl">uBlock Origin</p>
+      </div>
+      <div class="flex flex-col align-middle items-center gap-2">
+        <img class="w-24" src="/img/adblockers/privacy-badger.png" alt="privacy-badger" />
+        <p class="text-2xl">Privacy Badger</p>
+      </div>
+      <div class="flex flex-col align-middle items-center gap-2">
+        <img class="w-24" src="/img/adblockers/ghostery.png" alt="ghostery" />
+        <p class="text-2xl">Ghostery</p>
+      </div>
+      <div class="flex flex-col align-middle items-center gap-2">
+        <img class="w-24" src="/img/adblockers/adguard.png" alt="adguard" />
+        <p class="text-2xl">AdGuard</p>
+      </div>
+      <div class="flex flex-col align-middle items-center gap-2">
+        <img class="w-24" src="/img/adblockers/adlock.png" alt="adlock" />
+        <p class="text-2xl">AdLock</p>
+      </div>
     </div>
   </section>
 
   <section id="browsers">
     <h3 class="text-left">Browsers:</h3>
     <div class="grid grid-cols-3 grid-rows-3 justify-items-center w-full gap-8">
-      <img class="w-24" src="/img/browsers/chrome.png" alt="chrome" />
-      <img class="w-24" src="/img/browsers/safari.png" alt="safari" />
-      <img class="w-24" src="/img/browsers/edge.png" alt="edge" />
-      <img class="w-24" src="/img/browsers/firefox.png" alt="firefox" />
-      <img class="w-24" src="/img/browsers/brave.png" alt="brave" />
-      <img class="w-24" src="/img/browsers/librewolff.png" alt="librewolff" />
-      <img class="w-24" src="/img/browsers/mullvad.png" alt="mullvad" />
-      <img class="w-24" src="/img/browsers/tor.png" alt="tor" />
+      <div class="flex flex-col align-middle items-center gap-2">
+        <img class="w-24" src="/img/browsers/chrome.png" alt="chrome" />
+        <p class="text-2xl">Chrome</p>
+      </div>
+      <div class="flex flex-col align-middle items-center gap-2">
+        <img class="w-24" src="/img/browsers/safari.png" alt="safari" />
+        <p class="text-2xl">Safari</p>
+      </div>
+      <div class="flex flex-col align-middle items-center gap-2">
+        <img class="w-24" src="/img/browsers/edge.png" alt="edge" />
+        <p class="text-2xl">Edge</p>
+      </div>
+      <div class="flex flex-col align-middle items-center gap-2">
+        <img class="w-24" src="/img/browsers/firefox.png" alt="firefox" />
+        <p class="text-2xl">Firefox</p>
+      </div>
+      <div class="flex flex-col align-middle items-center gap-2">
+        <img class="w-24" src="/img/browsers/brave.png" alt="brave" />
+        <p class="text-2xl">Brave</p>
+      </div>
+      <div class="flex flex-col align-middle items-center gap-2">
+        <img class="w-24" src="/img/browsers/librewolff.png" alt="librewolff" />
+        <p class="text-2xl">LibreWolff</p>
+      </div>
+      <div class="flex flex-col align-middle items-center gap-2">
+        <img class="w-24" src="/img/browsers/mullvad.png" alt="mullvad" />
+        <p class="text-2xl">Mullvad</p>
+      </div>
+
+      <div class="flex flex-col align-middle items-center gap-2">
+        <img class="w-24" src="/img/browsers/tor.png" alt="tor" />
+        <p class="text-2xl">Tor</p>
+      </div>
     </div>
   </section>
 
